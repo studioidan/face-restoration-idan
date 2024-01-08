392e027a595ea3c803e13be07f2603cda18ecc36
idan <studioidan@gmail.com>
2024-01-08 22:12:48 +0200

    logs

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from inference_codeformer import start
 import cv2
+import time
+
 
 # img = cv2.imread('tests/t2.png', cv2.IMREAD_COLOR)
+start_time = time.time()
 img = start('t1.png')
 cv2.imwrite('restored.png', img)
+print(f'job done {int(time.time() - start_time)} secs')
+
