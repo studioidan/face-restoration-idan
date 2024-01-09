094095678a9fcec46f80a1cf1538a0e972a9a9a8
idan <studioidan@gmail.com>
2024-01-09 12:12:01 +0200

    logs

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import cv2
 import time
 
 
-# img = cv2.imread('tests/t2.png', cv2.IMREAD_COLOR)
+
 start_time = time.time()
 img = start('t1.png')
 cv2.imwrite('restored.png', img)
 print(f'job done {int(time.time() - start_time)} secs')
+
+
+start_time = time.time()
+img = cv2.imread('tests/t2.png', cv2.IMREAD_COLOR)
+img2 = start('t1.png')
+cv2.imwrite('restored2.png', img2)
+print(f'job done {int(time.time() - start_time)} secs')
+
 
